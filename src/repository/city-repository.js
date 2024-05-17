20eb1071f50fe2ed062702b31072e0773e691318
CVJaswanthiReddy <138797809+CVJaswanthiReddy@users.noreply.github.com>
2024-05-17 07:34:15 +0530

    Update city-repository.js

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -47,7 +47,7 @@ class CityRepository{
     async getCity({cityId}){
         try {
             const city= await City.findBypk(cityId);
-            return City;
+            return city;
         } catch (error) {
             console.log("something went wrong in the repository layer");
             throw{error};
