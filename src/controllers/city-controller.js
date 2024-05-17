e75041b47b420137d2a5f54ab22b0d2d7af370ca
CVJaswanthiReddy <138797809+CVJaswanthiReddy@users.noreply.github.com>
2024-05-17 14:33:18 +0530

    city-controller.js

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -47,7 +47,7 @@ const destroy = async(req,res) =>{
 //GET -> /city/:id
 const get = async(req,res) =>{
     try {
-        const response = await cityService.deleteCity(req.params.id);
+        const response = await cityService.getCity(req.params.id);
         return res.status(200),json({
             data:response,
             success:true,
@@ -69,7 +69,7 @@ const get = async(req,res) =>{
 //patch -> /city/:id ->req.body
 const update = async(req,res) =>{
     try {
-        const response = await cityService.deleteCity(req.params.id,req.body);
+        const response = await cityService.updateCity(req.params.id,req.body);
         return res.status(200),json({
             data:response,
             success:true,
