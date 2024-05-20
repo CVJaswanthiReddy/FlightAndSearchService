94cb5280239944038fd7fc401f316198522ca03f
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-05-20 14:11:29 +0530

    Added flights search crudwith few filters

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -16,12 +16,33 @@ const create= async(req,res)=>{
         return res.status(500).json({
             data:{},
             success:false,
-            message: " Not created a city",
+            message: " Not created a flight",
+            err:error
+        });
+    }
+}
+
+const getAll = async(req,res) => {
+    try {
+        const flights= await flightService.getAllFlightData(req.query);
+        return res.status(200).json({
+            data:flights,
+            success:true,
+            err:{},
+            message:'successfully fetched a flight'
+       });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message: " Not abe to fetch the flight",
             err:error
         });
     }
 }
 
 module.exports={
-    create
+    create,
+    getAll
 }
